ee214628045098f7b340f8740035da38b9893486
Chris <christopherdakin1@gmail.com>
2016-12-21 01:17:48 -0600

    atempt 8...

--- server/controllers/dreams-controller.js
+++ server/controllers/dreams-controller.js
@@ -3,12 +3,20 @@ var Dream = require('../models/dream');
 module.exports.create = function(req, res) {
   var dream = new Dream(req.body)
   dream.save(function(err, result) {
-    res.send(result);
+    if(err) {
+      console.log(err)
+    } else {
+      res.send(result);
+    }
   })
 }
 
 module.exports.list = function(req, res) {
   Dream.find({}, function (err, results) {
-    res.send(results);
+    if(err) {
+      console.log(err)
+    } else {
+      res.send(results);
+    }
   })
 }
