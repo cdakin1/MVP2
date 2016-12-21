fd9f3b3e20192c1108fceff6b3e6159d522d3a7a
Chris <christopherdakin1@gmail.com>
2016-12-21 01:12:18 -0600

    attempt6...changed dreams controller

--- server/controllers/dreams-controller.js
+++ server/controllers/dreams-controller.js
@@ -3,12 +3,12 @@ var Dream = require('../models/dream');
 module.exports.create = function(req, res) {
   var dream = new Dream(req.body)
   dream.save(function(err, result) {
-    res.json(result);
+    res.send(result);
   })
 }
 
 module.exports.list = function(req, res) {
   Dream.find({}, function (err, results) {
-    res.json(results);
+    res.send(results);
   })
 }
